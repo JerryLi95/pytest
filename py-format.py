f156a6ad7ad1970acb46c6831964b9ed245ab0a0
jerry <944673842@qq.com>
2022-01-02 21:20:36 +0800

    测试

---
+++ py-format.py
@@ -0,0 +1,16 @@
+print(123)
+print(123)
+print(123)
+print(123)
+print(123)
+print(123)
+for letter in 'Python':     # 第一个实例
+   print("当前字母: %s" % letter)
+fruits = ['banana', 'apple',  'mango']
+for fruit in fruits:        # 第二个实例
+   print ('当前水果: %s'% fruit)
+def dasd(x,y)
+    x=x+y
+    return x
+ 
+print ("Good bye!")
---
+++ pytest.py
@@ -0,0 +1,18 @@
+import os
+
+
+def asd():
+    a = 1
+    b = 2
+    print(a, b)
+
+
+asd()
+
+for x in [1, 10, 2]:
+    print(x)
+a = 4
+
+
+def test():
+    print("test")
